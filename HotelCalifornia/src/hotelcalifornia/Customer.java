43ab0806635ab2c9d9b6a01de233aedb981ab481
Produkten <Prototyp av produkten>
2019-01-08 14:34:25 +0100

    Customer updated

--- HotelCalifornia/src/hotelcalifornia/Customer.java
+++ HotelCalifornia/src/hotelcalifornia/Customer.java
@@ -5,11 +5,63 @@
  */
 package hotelcalifornia;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Helena
  */
 public class Customer {
+    
+    String ssn;
+    String name;
+    String address;
+    String telephoneNumber;
+    
+    ArrayList<Booking> bookings;
+    
+    public Customer(String ssn, String name, String address, String telephoneNumber){
+        this.ssn = ssn;
+        this.name = name;
+        this.address = address;
+        this.telephoneNumber = telephoneNumber;
+        bookings = new ArrayList();
+    }
+    
+    public String getSsn(){
+    return ssn;
+    }
+    
+    public String getName(){
+    return name;
+    }
+    
+    public String getAddress(){
+    return address;
+    }
+    
+    public String getTelephoneNumber(){
+    return telephoneNumber;
+    }
+    
+    public ArrayList<Booking> getBookings (){
+    return bookings;
+    }
+    
+    public void setAdress(String newAddress){
+    address = newAddress;
+    }
+    
+    public void setTelephoneNumber (String newTel){
+    telephoneNumber = newTel;
+    }
+    
+    public void addBooking(Booking b){
+        bookings.add(b);
+    }
+    
+    
+    
     //attribut personnummer, namn, adress, telefon (String)
     //ArrayList av Booking
     //andra attribute
