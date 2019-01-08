1969f86a943e4a20632a51de0355a9ce81adfcf5
NewDay <NewDay@NewDay-PC>
2019-01-08 14:37:20 +0100

    Attribute,construct,getter,setter

--- src/hotelcalifornia/Booking.java
+++ src/hotelcalifornia/Booking.java
@@ -1,7 +1,50 @@
 
 package hotelcalifornia;
 
+import java.util.ArrayList;
+
 
 public class Booking {
+    int bookingId;
+    String checkInDate;
+    String checkOutDate;
+    double totalPrice;
+    
+    ArrayList<Room> rooms;
+    
+    public Booking(int bookingId,String checkInDate,String checkOutDate,double totalPrice){
+    
+        this.bookingId = bookingId;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.totalPrice = totalPrice;
+        rooms = new ArrayList();
+    }
+   
+    public int getBookingId(){
+        return bookingId;
+        }
+    public String getCheckInDate(){
+        return checkInDate;
+        }
+    public String getCheckOutDate(){
+        return checkOutDate;
+    }
+    public double getTotalPrice(){
+        return totalPrice;
+    }
+    public ArrayList<Room> getRooms(){
+        return rooms;
+    }
+    public void addRoom(Room R){
+        rooms.add(R);
+    }
+    public void setCheckOutDate(String newDate){
+        checkOutDate = newDate;
+    }
+    public void setTotalPrice(double newPrice){
+        totalPrice = newPrice;
+    }
+    
     
 }
