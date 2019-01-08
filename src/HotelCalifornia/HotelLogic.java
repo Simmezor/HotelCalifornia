9db0b8c4b4ef6a09d9a880ef9b4adc5e5905ac43
Andreas <Andreas@LAPTOP-MD25NQMN>
2019-01-08 14:33:59 +0100

    uppdaterade med meny

--- src/HotelCalifornia/HotelLogic.java
+++ src/HotelCalifornia/HotelLogic.java
@@ -35,41 +35,199 @@ public class HotelLogic {
     
      // TODO code application logic here
         
-        boolean running = true;
-        
-        Scanner sc = new Scanner(System.in);
-        
-        String input;
-   
-                 System.out.println("Welcome to Hotel California");
-                 
-                 System.out.println("Here are your options:");
-                 
+       int ShowMenu;
+Scanner sc = new Scanner(System.in);
+while(true){  
+System.out.println("1 = Rooms\n"
+        + "2 = Booking\n"
+        + "3 = Customer\n"
+        + "4 = fdfdsfdsf\n"
+        + "5 = sdfdsfdsfsdf\n");
+        ShowMenu = sc.nextInt();
+      
+switch (ShowMenu)
+{
+
+case 1:
+    roomChoice();
+break;
+
+case 2:
+    bookingChoice();
+break;
+
+case 3:
+   customerChoice();
+break;
+
+case 4:
+    testMenu();
+    break;
+    
+case 5:
+    testMenu();
+    break;
+    
+default:
+System.out.println("Error: Pick 1 - 5 and try again!");
+break;
+ }
+ }
 
         
-        while(running == true){
-                 System.out.println("1. Testing print function. \n");
-                 
-                 System.out.println("Type 'exit' to close the application.");
-                 
-            input = sc.next();
+    }
         
-            System.out.println(input);
-            
-            if(input.matches("exit")){
-            running = false;
-            break;
-            }
+        public static void testMenu(){ System.out.println("\n" +"\u001B[32m" + "Working master" + "\u001B[0m" + "\n");}
         
-            if(input.matches("1")){
+        public static void roomChoice(){
             
-                System.out.println("This is a test print.");
-                
-            }
+            int choice;
+            boolean asking = true;
+Scanner sc = new Scanner(System.in);
+while(asking == true){  
+System.out.println("1 = Show free rooms\n"
+        + "2 = Add new room\n"
+        + "3 = Remove room\n"
+        + "4 = Check in customer\n"
+        + "5 = Check out customer\n"
+        + "6 = Return to main menu");
+        choice = sc.nextInt();
+      
+switch (choice)
+{
+
+case 1:
+    System.out.println("Show free rooms");
+    testMenu();
+break;
+
+case 2:
+    System.out.println("Add new room");
+    testMenu();
+break;
+
+case 3:
+    System.out.println("Remove room");
+    testMenu();
+break;
+
+case 4:
+    System.out.println("Check in customer");
+    testMenu();
+    break;
+    
+case 5:
+    System.out.println("Check out customer");
+    testMenu();
+    break;
+case 6:
+    System.out.println("Returning to main menu");
+    asking = false;
+break;    
+    
+default:
+System.out.println("Error: Pick 1 - 6 and try again!");
+break;
+ }
+ }
             
         }
         
-    }
+        public static void customerChoice(){
+             int choice;
+            boolean asking = true;
+Scanner sc = new Scanner(System.in);
+while(asking == true){  
+System.out.println("1 = Show Cusomers\n"
+        + "2 = Add new Customer\n"
+        + "3 = Remove Customer\n"
+        + "4 = Edit customer\n"
+        + "5 = Return to main menu");
+        choice = sc.nextInt();
+      
+switch (choice)
+{
+
+case 1:
+    System.out.println("Show Customer");
+    testMenu();
+break;
+
+case 2:
+    System.out.println("Add new Customer");
+    testMenu();
+break;
+
+case 3:
+    System.out.println("Remove Customer");
+    testMenu();
+break;
+
+case 4:
+    System.out.println("Edit customer");
+    testMenu();
+    break;
+
+case 5:
+    System.out.println("Returning to main menu");
+    asking = false;
+break;    
+    
+default:
+System.out.println("Error: Pick 1 - 5 and try again!");
+break;
+ }
+ }
+            
+        }
+        
+        public static void bookingChoice(){
+             int choice;
+            boolean asking = true;
+Scanner sc = new Scanner(System.in);
+while(asking == true){  
+System.out.println("1 = Show Bookings\n"
+        + "2 = Add new Booking\n"
+        + "3 = Remove Booking\n"
+        + "4 = Edit Booking\n"
+        + "5 = Return to main menu");
+        choice = sc.nextInt();
+      
+switch (choice)
+{
+
+case 1:
+    System.out.println("Show Bookings");
+    testMenu();
+break;
+
+case 2:
+    System.out.println("Add new Booking");
+    testMenu();
+break;
+
+case 3:
+    System.out.println("Remove Booking");
+    testMenu();
+break;
+
+case 4:
+    System.out.println("Edit Booking");
+    testMenu();
+    break;
+
+case 5:
+    System.out.println("Returning to main menu");
+    asking = false;
+break;    
+    
+default:
+System.out.println("Error: Pick 1 - 5 and try again!");
+break;
+ }
+ }
+            
+        }
     
     public void getRoom(){}
     public void getRooms(){}
