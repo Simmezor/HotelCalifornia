db5842358d95999e5a2528594cb4022daad117e1
Andreas <Andreas@LAPTOP-MD25NQMN>
2019-01-09 14:02:04 +0100

    Lagt in GUI meny och funktioner

---
+++ HotelCalifornia/src/hotelcalifornia/Customer.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelcalifornia;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Helena
+ */
+public class Customer {
+    
+    String ssn;
+    String name;
+    String address;
+    String telephoneNumber;
+    
+    ArrayList<Booking> bookings;
+    
+    public Customer(String ssn, String name, String address, String telephoneNumber){
+        this.ssn = ssn;
+        this.name = name;
+        this.address = address;
+        this.telephoneNumber = telephoneNumber;
+        bookings = new ArrayList();
+    }
+    
+    public String getSsn(){
+    return ssn;
+    }
+    
+    public String getName(){
+    return name;
+    }
+    
+    public String getAddress(){
+    return address;
+    }
+    
+    public String getTelephoneNumber(){
+    return telephoneNumber;
+    }
+    
+    public ArrayList<Booking> getBookings (){
+    return bookings;
+    }
+    
+    public void setAdress(String newAddress){
+    address = newAddress;
+    }
+    
+    public void setTelephoneNumber (String newTel){
+    telephoneNumber = newTel;
+    }
+    
+    public void addBooking(Booking b){
+        bookings.add(b);
+    }
+    
+    
+    
+    //attribut personnummer, namn, adress, telefon (String)
+    //ArrayList av Booking
+    //andra attribute
+    //Contructor
+    //getter setter
+    
+}
---
+++ HotelCalifornia/src/hotelcalifornia/HotelLogic.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelcalifornia;
+
+import java.util.ArrayList;
+
+public class HotelLogic {
+    
+    // vi skapar en arraylist av typen Customer
+    ArrayList<Customer> customers;
+    // vi skapar en arraylist av typen Room
+    ArrayList<Room> rooms;
+    
+    //constructor
+    public HotelLogic(){
+    customers = new ArrayList();
+    
+    }
+    
+    //en metod per backlog (kan vara fler)
+    //namn på metoderna ska visa vad de gör
+    //metod för show menu
+    //menu = loop + switch case
+    
+    
+    
+    
+}
