8582e20b6b031be791144775916034a28cd950db
Simmezor <45822655+Simmezor@users.noreply.github.com>
2019-01-10 18:20:22 +0100

    Update HotelCalifornia.java

--- src/HotelCalifornia/HotelCalifornia.java
+++ src/HotelCalifornia/HotelCalifornia.java
@@ -3,7 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package HotelCalifornia;
+package hotelcalifornia;
+
+import java.util.ArrayList;
 
 
 /**
@@ -14,14 +16,72 @@ public class HotelCalifornia {
 
 
     
+   public static void printRoomInfo(Room r){
+   
+       System.out.println("RoomNumber: "+r.getRoomNumber());
+       System.out.println("Beds: "+r.getNumberOfBeds());
+       System.out.println("Has Balcony: "+r.isHasBalcony());
+       System.out.println("Price: "+r.getPricePerNight());
+       System.out.println("Booked: "+r.isIsBooked());
+       
+       System.out.println("");
+       
+       
+   }
+    
     
     public static void main(String[] args) {
        
-        HotelLogic myhotel = new HotelLogic();
+        HotelLogic myhotelLogic = new HotelLogic();
         
         System.out.println("Test");
         
-        myhotel.showMenu();
-    }
-    
-}
+        //myhotelLogic.addRoom();
+        
+         ArrayList <Customer> customers;
+         ArrayList <Room> rooms;
+        
+         customers = new ArrayList();
+         rooms = new ArrayList();
+         
+         int numberofbeds = 0;
+         double price = 0;
+         boolean booked = false;
+         boolean balcony = false;
+         
+         
+         for (int i = 0; i < 10; i++) {
+             
+             if(i % 2 == 0){
+             numberofbeds = 1;
+             price = 79.49;
+             }else{
+              numberofbeds = 2;
+              price = 129.49;
+             }
+             
+                          if(i % 3 == 0){
+             booked = true;
+             }else{
+            booked = false;
+             }
+                          
+             if(i % 4 == 0){
+             balcony = true;
+             }else{
+            balcony = false;
+             }
+             
+             
+            rooms.add(new Room(i+1, numberofbeds,balcony,price, booked ));
+            
+            printRoomInfo(rooms.get(i));
+        }
+         
+      
+         
+     } // end of main  
+        
+} // end of class
+
+
