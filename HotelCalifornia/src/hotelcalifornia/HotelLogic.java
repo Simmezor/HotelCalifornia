35ea13474ea84fca3c636909e8a4b6918568466b
Simmezor <45822655+Simmezor@users.noreply.github.com>
2019-01-10 11:34:20 +0100

    Update HotelLogic.java

--- HotelCalifornia/src/hotelcalifornia/HotelLogic.java
+++ HotelCalifornia/src/hotelcalifornia/HotelLogic.java
@@ -52,7 +52,96 @@ public class HotelLogic {
     //metod för show menu
     //menu = loop + switch case
     
-    
+   public void addRoom(){
+         Scanner in = new Scanner(System.in);
+
+         boolean gettinginput = true;
+         
+         int temproomnum =-1;
+         int beds = -1;
+         
+         String balcony = "-1";
+         boolean balconybool = false;
+         double price = -0;
+         
+         while(gettinginput) {
+                  try{
+         System.out.println("Whats the room number: ");    
+         temproomnum = in.nextInt();      
+         gettinginput =false;
+                  }catch( Exception e){
+                      System.out.println("invalid input");
+                      in.next();
+                           
+            }
+         }
+         gettinginput = true;
+                  while(gettinginput) {
+                  try{
+         System.out.println("How many beds does the room have?: ");    
+         beds = in.nextInt();    //Needs to be in a try catch 
+         gettinginput =false;
+                  }catch( Exception e){
+                      System.out.println("invalid input");
+                      in.next();
+                           
+            }
+         }
+                  
+                    gettinginput = true;
+                   while(gettinginput) {
+                
+         System.out.println("Does the room have a balcony?: ");    
+         balcony = in.next();    //Needs to be in a try catch 
+         
+          switch(balcony){
+
+              case "yes":
+                 balconybool = true;
+                    gettinginput =false;
+                    break;
+              case "Yes":
+                 balconybool = true;
+                    gettinginput =false;
+                   break;
+              case "no":
+                 balconybool = false;
+                    gettinginput =false;
+                   break;
+              case "No":
+                 balconybool = false;
+                    gettinginput =false;
+                   break;
+              default:
+                  System.out.println("invalid input");
+
+                   break;
+          }
+              
+            
+         }
+                    gettinginput = true;
+
+                                    while(gettinginput) {
+                  try{
+         System.out.println("What is the price for the room?: ");    
+         price = in.nextDouble();    //Needs to be in a try catch 
+         gettinginput =false;
+                  }catch( Exception e){
+                      System.out.println("invalid input");
+                      in.next();
+                           
+            }
+         }
+         
+                              
+         
+         Room newRoom = new Room(temproomnum,beds,balconybool,price,false);     
+         
+         
+         rooms.add(newRoom);
+
+    }
     
     
 }
